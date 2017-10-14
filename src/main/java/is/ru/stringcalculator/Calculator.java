23fec6b047fc467ad9ae0fceb480cad801d738a6
unknown <thorirv15@ru.is>
2017-10-14 16:23:09 +0000

    Fixed and cleaned code in Calculator class

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,20 +1,32 @@
 package is.ru.stringcalculator;
 
 public class Calculator {
+
+	private static int toInt(int number){
+		return Integer.parseInt(number);
+	}
+
+	private static int sum(String numbers) {
+		int sum = 0;	
+		String [] numberArr = numbers.split(",");
+		
+		if(numberArr.length > 1) {
+			for(String s: numberArr) {
+				sum += toInt(s);
+			}
+			return sum;
+		}
+
+		return toInt(numbers);
+	}
+
 	public static int add(String numbers) {
 		if(numbers == "") {
 			return 0;
 		}
 
-		int sum = 0;	
-		String [] numberArr = numbers.split(",");
-		if(numberArr.length == 2) {
-				sum = Integer.parseInt(numberArr[0]) 
-					+ Integer.parseInt(numberArr[1]);
-								
-			return sum;
-		}
+		int sum = sum(numbers);
 
-		return Integer.parseInt(numbers);
+		return sum;
 	}
 }
