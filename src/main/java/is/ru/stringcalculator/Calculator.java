76ec76697f7d952311a0b4504850e1b1970b066b
unknown <thorirv15@ru.is>
2017-10-15 16:06:51 +0000

    Successful test for ignoring numbers above 1000 in calculation

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -12,7 +12,9 @@ public class Calculator {
 		
 		if(numberArr.length > 1) {
 			for(String s: numberArr) {
-				sum += toInt(s);
+				if(toInt(s) < 1000){
+					sum += toInt(s);
+				}
 			}
 			return sum;
 		}
@@ -20,13 +22,40 @@ public class Calculator {
 		return toInt(numbers);
 	}
 
-	public static int add(String numbers) {
-		if(numbers == "") {
-			return 0;
+	private static boolean validationForInput(String numbers) {
+		String [] numberArr = numbers.split(",|\n");
+		String [] invalidNumberArr = new String [numberArr.length];
+		boolean isNegNumber = false;
+		String errString = "";
+
+		for(int i = 0; i < numberArr.length; i++) {
+				if(toInt(numberArr[i]) < 0) {
+					invalidNumberArr[i] = numberArr[i];
+					errString += (invalidNumberArr[i] + ",");
+					isNegNumber = true;
+				} 
 		}
 
-		int sum = getSum(numbers);
+		if(isNegNumber) {
+			String errorString = errString.substring(0, errString.length() - 1);
+			throw new IllegalArgumentException("Negatives not allowed:" + errorString);
+		}
 
-		return sum;
+		return false;
+	}
+
+	public static int add(String numbers) {
+		if(numbers.isEmpty()) {
+				return 0;
+		}
+
+		if(!validationForInput(numbers)){
+			
+			int sum = getSum(numbers);
+
+			return sum;
+		}
+
+		return 0;
 	}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -1,6 +1,8 @@
 package is.ru.stringcalculator;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 public class CalculatorTest {
 
@@ -29,4 +31,30 @@ public class CalculatorTest {
 		assertEquals(16, Calculator.add("2,3,10\n1"));
 	}
 
+	// @Rule
+	// public ExpectedException thrown = ExpectedException.none(); 
+
+ //    @Test
+	// public void testStringWithNegNumbers() {
+		
+	// 	thrown.expect(IllegalArgumentException.class);
+	// 	thrown.expectMessage("Negatives not allowed: -1, -4, -9");
+	// 	Calculator.add("2, -1, -4, 3, 10\n1, -9");
+		
+	// }
+
+	// @Test
+	// public void testStringWithNegNumbers() {
+	// 	try {
+	// 		assertEquals(2, Calculator.add("2, -1, -4, 3, 10\n1, -9"));
+	// 	}
+	// 	catch (Exception e) {
+	// 		assertEquals("Negatives not allowed: -1, -4, -9", e.getMessage());
+	// 	}		
+	// }
+
+	@Test
+	public void testIfnumbersAbove1000AreIgnored() {
+		assertEquals(2, Calculator.add("1001,2"));
+	}
 }
